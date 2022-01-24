0fe50fa9a151630bc0d435b763fc90c1f8cfd420
Thms20 <74256749+Thms20@users.noreply.github.com>
2022-01-24 02:01:48 +0100

    Update Affichage.java

--- bird/Affichage.java
+++ bird/Affichage.java
@@ -2,19 +2,18 @@ package bird;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 
 import javax.swing.JPanel;
 
 /**
  * @author Thomas
  *
- * La classe Affichage correspond à la vue dans le modèle MVC. 
- * Elle décrit les formes utilisées dans l'affichage.
+ * La classe Affichage correspond Ã  la vue dans le modÃ¨le MVC. 
+ * Elle dÃ©crit les formes utilisÃ©es dans l'affichage.
  */
 
-public class Affichage extends JPanel {
-	public Etat etat = new Etat(this);
-	
+public class Affichage extends JPanel {	
 	public static final int largeur_panel = 800; /** Largeur du panel */
     public static final int hauteur_panel = 500; /** Hauteur du panel */
     
@@ -22,6 +21,8 @@ public class Affichage extends JPanel {
     public static int OVAL_Y = 200;
     public static final int OVAL_WIDTH = 50;  /** Largeur de l'oval */
     public static final int OVAL_HEIGHT = 100; /** Hauteur de l'oval */
+    
+    public Etat etat = new Etat(this);
     
     
     public Affichage() {
@@ -36,9 +37,15 @@ public class Affichage extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         g.drawOval(OVAL_X, OVAL_Y, OVAL_WIDTH, OVAL_HEIGHT);
+        
+        for(int i = 0; i < etat.getParcours().getPoints().size()-1; i++) {
+        	Point p1 = etat.getParcours().getPoints().get(i);
+        	Point p2 = etat.getParcours().getPoints().get(i + 1);
+        	g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
     }
     
-    /** Permet de changer la valeur centrale en ordonnée de l'oval de l'affichage */
+    /** Permet de changer la valeur centrale en ordonnÃ©e de l'oval de l'affichage */
     public void setOval_Y(int x) {
     	OVAL_Y = x;
     }
@@ -52,5 +59,7 @@ public class Affichage extends JPanel {
     public int getHauteurPanel() {
     	return hauteur_panel;
     }
+    
+    
 
 }
