c1ab795a4ededa9e0e922fb26448d26710e87490
Thms20 <74256749+Thms20@users.noreply.github.com>
2022-01-24 02:04:31 +0100

    Update Main.java

--- bird/Main.java
+++ bird/Main.java
@@ -7,16 +7,18 @@ import javax.swing.JFrame;
  * 
  * @author Thomas
  * 
- * La classe Main permet de relier les classes entre elles et contient le code lu et donc excécuté.
+ * La classe Main permet de relier les classes entre elles et contient le code lu et donc excÃ©cutÃ©.
  *
  */
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Affichage aff = new Affichage();
 		Etat etat = new Etat(aff);
 		Control control = new Control(etat, aff);
+		Vol vol = new Vol(control);
 		
 		JFrame fenetre = new JFrame("Main");
 		aff.addMouseListener(control);
@@ -25,6 +27,8 @@ public class Main {
 		fenetre.pack();
         fenetre.setVisible(true);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        vol.start();
 	} 
 
 }
